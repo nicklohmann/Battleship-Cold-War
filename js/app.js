da7256c2b868c0de9071496b9879438b51a27abf
nicholasL <nicklohmann@msn.com>
2023-03-28 17:16:05 -0500

    initial Pseudocode

---
+++ js/app.js
@@ -0,0 +1,14 @@
+/* Pseudocode:
+1. Create ship objects to place in the Game
+2. Store cached element references
+3. Load the game, initialize both boards to be empty, Game state will start with giving the user the option to place his ships, and then player 2 will after
+4. Handle player click to place each individual ship on the respective board
+5. Both boards will then become blank, unable to see the ships.
+
+6. Game state will be rendered to the user after each hit attempt
+
+7. Handle player click on each board to see if ship is hit
+ - Handle winner
+
+8. Create reset Functionality
+ */
