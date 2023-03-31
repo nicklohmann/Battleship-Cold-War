c2a22e5b72028678eb23476a0217715e8e909d7f
nicholasL <nicklohmann@msn.com>
2023-03-31 03:51:01 -0500

    class method bug fix

--- js/app.js
+++ js/app.js
@@ -4,37 +4,39 @@
 3. Load the game, initialize both boards to be empty, Game state will start with giving the user the option to place their ships, and then player 2 will after
 4. Handle player click to place each individual ship on the respective board
 5. Both boards will then become blank, unable to see the ships.
-
 6. Game state will be rendered to the user after each hit attempt
  -miss
  -hit
+ -hit and sunk
  -invalid
 7. Handle player click on each board to see if ship is hit
-- Handle winner
-
+- Handle winner when all 5 ships are sunk
 8. Create reset Functionality
  */
 let board
-
-class ship {
+class Ship {
   constructor(nation, length) {
     this.hitCount = 0
     this.isSunk = false
     this.nation = nation
     this.length = length
   }
-  sunk() {
+  destroyed() {
     this.isSunk = true
+    console.log('sunk');
   }
 }
-const carrierUSA = new ship('USA', 5)
-const battleshipUSA = new ship('USA', 4)
-const cruiserUSA = new ship('USA', 3)
-const submarineUSA = new ship('USA', 3)
-const destroyerUSA = new ship('USA', 2)
-const carrierSoviet = new ship('Soviet', 5)
-const battleshipSoviet = new ship('Soviet', 4)
-const cruiserSoviet = new ship('Soviet', 3)
-const submarineSoviet = new ship('Soviet', 3)
-const destroyerSoviet = new ship('Soviet', 2)
+const carrierUSA = new Ship('USA', 5)
+const battleshipUSA = new Ship('USA', 4)
+const cruiserUSA = new Ship('USA', 3)
+const submarineUSA = new Ship('USA', 3)
+const destroyerUSA = new Ship('USA', 2)
+const carrierSoviet = new Ship('Soviet', 5)
+const battleshipSoviet = new Ship('Soviet', 4)
+const cruiserSoviet = new Ship('Soviet', 3)
+const submarineSoviet = new Ship('Soviet', 3)
+const destroyerSoviet = new Ship('Soviet', 2)
+
+
+
 
