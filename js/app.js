77d9bebe4f8fb4d10534f638fda20644fb10cd22
nicholasL <nicklohmann@msn.com>
2023-03-30 23:37:17 -0500

    create ship class and update variables

--- js/app.js
+++ js/app.js
@@ -1,25 +1,40 @@
 /* Pseudocode:
 1. Create ship objects to place in the Game
 2. Store cached element references
-3. Load the game, initialize both boards to be empty, Game state will start with giving the user the option to place his ships, and then player 2 will after
+3. Load the game, initialize both boards to be empty, Game state will start with giving the user the option to place their ships, and then player 2 will after
 4. Handle player click to place each individual ship on the respective board
 5. Both boards will then become blank, unable to see the ships.
 
 6. Game state will be rendered to the user after each hit attempt
-
+ -miss
+ -hit
+ -invalid
 7. Handle player click on each board to see if ship is hit
- - Handle winner
+- Handle winner
 
 8. Create reset Functionality
  */
+let board
 
-const carrier1 = {isSunk: false, hitCount: 0 , length:5}
-const battleship1 = {isSunk: false, hitCount: 0 , length:4}
-const cruiser1 = {isSunk: false, hitCount: 0 , length:3}
-const submarine1 = {isSunk: false, hitCount: 0 , length:3}
-const destroyer1 = {isSunk: false, hitCount: 0 , length:2}
-const carrier2 = {isSunk: false, hitCount: 0 , length:5}
-const battleship2 = {isSunk: false, hitCount: 0 , length:4}
-const cruiser2 = {isSunk: false, hitCount: 0 , length:3}
-const submarine2 = {isSunk: false, hitCount: 0 , length:3}
-const destroyer2 = {isSunk: false, hitCount: 0 , length:2}
+class ship {
+  constructor(nation, length) {
+    this.hitCount = 0
+    this.isSunk = false
+    this.nation = nation
+    this.length = length
+  }
+  sunk() {
+    this.isSunk = true
+  }
+}
+const carrierUSA = new ship('USA', 5)
+const battleshipUSA = new ship('USA', 4)
+const cruiserUSA = new ship('USA', 3)
+const submarineUSA = new ship('USA', 3)
+const destroyerUSA = new ship('USA', 2)
+const carrierSoviet = new ship('Soviet', 5)
+const battleshipSoviet = new ship('Soviet', 4)
+const cruiserSoviet = new ship('Soviet', 3)
+const submarineSoviet = new ship('Soviet', 3)
+const destroyerSoviet = new ship('Soviet', 2)
+
